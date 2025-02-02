5fcbbefe0fd03d3fc814034d87eb4a9863b43347
George Pandelias <pandeliasg@gmail.com>
2025-02-02 11:35:40 +0200

    Implement header component

--- padashop/components/shared/header/index.tsx
+++ padashop/components/shared/header/index.tsx
@@ -7,18 +7,28 @@ import { APP_NAME } from '@/lib/constants';
 
 
 const Header = () => {
-    return (<header className='w-full border-b'>
-        
-        <div className="wrapper flex-between">
-            <div className="flex-start">
-
-                <Link href="/" className='flex-start'>
-                    <Image src='images/logo.svg' alt={`${APP_NAME} logo`} height={48} width={48} priority={true} />
-                    <span className="hidden font-bold text-2xl lg:block"></span>
-                </Link>
+    return (
+        <header className='w-full border-b'>    
+            <div className="wrapper flex-between">
+                <div className="flex-start">
+                    <Link href="/" className='flex-start'>
+                        <span className="hidden lg:inline-block font-bold text-2xl ml-3 text-teal-800">{APP_NAME}</span>
+                    </Link>
                 </div>
-        </div>
-    </header>  );
+                <div className='space-x-2'>
+                    <Button asChild variant='ghost'>
+                        <Link href='/cart'>
+                        <ShoppingCart /> Καλάθι</Link>
+                    </Button>
+                    <Button asChild className='bg-teal-800 hover:bg-teal-600'>
+                        <Link href='/login'>
+                        <UserIcon /> Σύνδεση / Εγγραφή
+                        </Link>
+                    </Button>
+                </div>
+            </div>
+        </header>
+    );
 }
  
 export default Header;
