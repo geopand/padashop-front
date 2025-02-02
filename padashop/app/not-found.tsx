f9a87ff00699a1b4d73465fbec607711e36d94ea
George Pandelias <pandeliasg@gmail.com>
2025-02-02 17:47:28 +0200

    add house icon in not-found page

--- padashop/app/not-found.tsx
+++ padashop/app/not-found.tsx
@@ -1,25 +1,21 @@
 'use client';
 import Link from 'next/link';
+import { House } from 'lucide-react';
 
 const NotFoundPage = () => {
     return (  
-        <section
-        style={{
-          display: "grid",
-          placeItems: "center",
-          textAlign: "center",
-          gap: "var(--space-sm)",
-          margin: "100px 0px 500px 0px",
-        }}
-      >
-        <p className="text-black-900 text-5xl mb-4">Χμ.... </p>
-        <p className="text-black-900 text-3xl mb-4"> Η σελίδα δεν βρέθηκε</p>
+        <section 
+        className='flex h-screen flex-col items-center  mt-28'>
+        <p className="text-black-900 text-5xl mb-4">Χμ....</p>
+        <p className="text-black-900 text-3xl mb-4">Η σελίδα δεν βρέθηκε</p>
 
         <p className='text-black-900 text-9xl'>404</p>
         <div className="mt-10"></div>
-
-        <Link href='/' className='text-teal-900 text-3xl hover:text-teal-700'>Mεταβείτε στην κύρια σελίδα</Link>
-           
+          <Link
+            href='/'
+            className='text-teal-900 text-3xl hover:text-teal-700'>
+              <House className='inline' /> Mεταβείτε στην κύρια σελίδα
+          </Link>   
       </section>
      );
 }
